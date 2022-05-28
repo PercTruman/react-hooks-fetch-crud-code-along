52b4d621559d1c85aa7c7e8e4541ce08e54b9a18
PercTruman <drshawnhart@gmail.com>
2022-05-28 10:59:42 -0500

    completing CRUD codealong

--- src/components/ShoppingList.js
+++ src/components/ShoppingList.js
@@ -11,8 +11,26 @@ function ShoppingList() {
     fetch('http://localhost:4000/items')
       .then(res=>res.json())
       .then(items =>setItems(items))
-  },[])
+  },[]);
 
+  function handleDeleteItem(deletedItem){
+    console.log("In ShoppingCart:", deletedItem);
+  }
+
+  function handleUpdateItem(updatedItem){
+    const updatedItems=items.map((item)=>{
+      if (item.id===updatedItem.id){
+        return updatedItem;
+      }else {
+        return item;
+      }
+    });
+    setItems(updatedItems);
+  }
+  function handleDeleteItem(deletedItem){
+    const updatedItems= items.filter((item)=>item.id !==deletedItem.id);
+    setItems(updatedItems);
+  }
   function handleAddItem(newItem){
    setItems([...items, newItem]);
   }
@@ -36,7 +54,7 @@ function ShoppingList() {
       />
       <ul className="Items">
         {itemsToDisplay.map((item) => (
-          <Item key={item.id} item={item} />
+          <Item key={item.id} item={item} onUpdateItem={handleUpdateItem} onDeleteItem={handleDeleteItem}/>
         ))}
       </ul>
     </div>
